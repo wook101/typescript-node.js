697d21609e3e204098a9b061555cd4b7a07b1f3e
wook101 <ehddnr1022@gmail.com>
2021-08-07 18:51:17 +0900

    express서버 생성

--- 프로젝트/ts-project/index.ts
+++ 프로젝트/ts-project/index.ts
@@ -4,10 +4,11 @@ import {Request, Response, NextFunction, Application } from 'express';
 const app:Application = express();
 const prod:boolean = process.env.NODE_ENV === 'production';
 
+app.set('port', prod ? process.env.PORT : 3000);
 app.get('/',(req:Request, res:Response, next:NextFunction)=>{
-    res.send("헬로우!!");
+    res.send("헬로우! 타입스크립트");
 });
 
-app.listen(prod ? process.env.PORT : 3000,()=>{
-    console.log(`Server running !! port : ${process.env.PORT}`);
+app.listen(app.get('port'),()=>{
+    console.log(`Server running on port : ${app.get('port')} !!`);
 });
